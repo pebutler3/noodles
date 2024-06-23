af1a7c3510ed8aa8371cfaa6b1fea6becd75a001
P.E. Butler III <p.e.butler3@gmail.com>
2024-06-22 22:19:26 -0600

    feat: add code examples

--- src/_components/containers/ButtonContainer.vue
+++ src/_components/containers/ButtonContainer.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import DefaultButtons from '../DefaultButtons.vue';
 import OutlinedButtons from '../OutlinedButtons.vue';
+import { dismissableButtons, standardButtons, standardButtonsOutlined } from '@/utils/consts';
+import InlineCode from '@/_components/InlineCode.vue';
 </script>
 
 <template>
@@ -10,38 +12,32 @@ import OutlinedButtons from '../OutlinedButtons.vue';
       <DefaultButtons />
       <div class="w-100">
         <section class="d-flex flex-wrap gap-05">
+          <pre class="w-100 d-grid">
+            <code class="language-js">
+              {{ standardButtons() }}
+            </code>
+          </pre>
           <OutlinedButtons />
           <pre class="w-100 d-grid">
             <code class="language-js">
-            {{`
-              // Standard Button
-              <Button title="Primary" />
-              // Outlined Button
-              <Button title="Primary "kind="primary--outlined" />
-            `}}
+              {{ standardButtonsOutlined() }}
             </code>
           </pre>
         </section>
       </div>
       <div class="w-100 mt-1">
         <h3 class="m-0 mb-1">Dismissable</h3>
+        <p>Using the available <InlineCode code="slot"/> in the <InlineCode code="Button" /> we can pass in the <InlineCode code="DismissButton" /></p>
         <section class="d-flex flex-wrap gap-05">
           <DefaultButtons :dismissable="true" />
           <div class="w-100">
             <section class="d-flex flex-wrap gap-05">
               <OutlinedButtons :dismissable="true" />
-              <div class="code-wrapper">
-                <pre class="w-100 d-grid">
-                  <code class="language-js">
-                  {{`
-                    // Standard Dismissable Button
-                    <Button title="Primary" kind="primary">
-                      <DismissButton />
-                    </Button>
-                  `}}
-                  </code>
-                </pre>
-              </div>
+              <pre class="w-100 d-grid">
+                <code class="language-js">
+                {{ dismissableButtons() }}
+                </code>
+              </pre>
             </section>
           </div>
         </section>
