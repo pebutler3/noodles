ad40ab881a022be672353f35f45891673c6bda65
P.E. Butler III <p.e.butler3@gmail.com>
2024-06-22 22:17:11 -0600

    refactor: simplify badge/button props

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,41 +1,20 @@
 <script lang="ts" setup>
-const props = withDefaults(defineProps<{
-  classes?: string[] | null
+interface BadgeProps {
+  text?: string
   title?: string
-  kind?: (
-    'primary' |
-    'primary--outlined' |
-    'secondary' |
-    'secondary--outlined' |
-    'success' |
-    'success--outlined' |
-    'danger' |
-    'danger--outlined' |
-    'warning' |
-    'warning--outlined' |
-    'info' |
-    'info--outlined' |
-    'link'
-  )
-  dismissable?: boolean
-}>(), {
-  kind: 'primary',
-})
+  outlined?: boolean
+}
+
+defineProps<BadgeProps>()
 
 const emit = defineEmits<{
   (e: 'badge-click', event: Event): void
 }>()
-
-let badgeClasses = `badge badge-${props.kind}`
-
-if (props.dismissable) {
-  badgeClasses += ' badge--dismissable'
-}
 </script>
 
 <template>
-  <span :class="classes || badgeClasses" @click="emit('badge-click', $event)">
-    {{ title || 'Click Me' }}
+  <span @click="emit('badge-click', $event)">
+    {{ text || 'Click Me' }}
     <slot />
   </span>
 </template>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,41 +1,18 @@
 <script lang="ts" setup>
-const props = withDefaults(defineProps<{
-  classes?: string[] | null
-  title?: string
-  kind?: (
-    'primary' |
-    'primary--outlined' |
-    'secondary' |
-    'secondary--outlined' |
-    'success' |
-    'success--outlined' |
-    'danger' |
-    'danger--outlined' |
-    'warning' |
-    'warning--outlined' |
-    'info' |
-    'info--outlined' |
-    'link'
-  )
-  dismissable?: boolean
-}>(), {
-  kind: 'primary',
-})
+interface ButtonProps {
+  text?: string
+}
+
+defineProps<ButtonProps>()
 
 const emit = defineEmits<{
   (e: 'btn-click', event: Event): void
 }>()
-
-let btnClass = `btn btn-${props.kind}`
-
-if (props.dismissable) {
-  btnClass += ' btn--dismissable'
-}
 </script>
 
 <template>
-  <button :class="classes || btnClass" @click="emit('btn-click', $event)">
-    {{ title || 'Click Me' }}
+  <button @click="emit('btn-click', $event)">
+    {{ text || 'Click Me' }}
     <slot />
   </button>
 </template>
