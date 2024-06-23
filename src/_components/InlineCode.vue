5b76e308d3634f5152f10d0da0ce815da532f5e0
P.E. Butler III <p.e.butler3@gmail.com>
2024-06-22 22:16:31 -0600

    feat: create InlineCode component for simple inline usage

---
+++ src/_components/InlineCode.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{
+  code: string
+}>()
+</script>
+
+<template>
+  <span>{{ code }}</span>
+</template>
+
+<style lang="scss" scoped>
+span {
+  background-color: #2d2d2d;
+  border-radius: 5px;
+  color: #cc99cd;
+  font-weight: bold;
+  padding: 2px 5px;
+}
+</style>
