304e8b6b6faec2e539139ca54316ef1e45cc63fb
P.E. Butler III <p.e.butler3@gmail.com>
2024-06-22 22:14:53 -0600

    created Table internal components

---
+++ src/_components/Table/Table.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import TableHeaderOrData from './TableHeaderOrData.vue';
+
+interface TableData {
+  prop: string
+  type: string
+  default: string
+}
+
+defineProps<{
+  headers?: string[] | null
+  tableData?: TableData[]
+}>()
+</script>
+
+<template>
+  <table class="w-100">
+    <thead>
+      <tr>
+        <TableHeaderOrData v-for="header in headers" :header="header" />
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="data in tableData" :key="data.prop">
+        <td v-for="d in data">
+          {{ d }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style lang="scss" scoped>
+table {
+  margin-top: 0.5rem;
+  border-collapse: collapse;
+}
+</style>
---
+++ src/_components/Table/TableHeaderOrData.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+defineProps<{
+  header?: string | null
+  td?: string | null 
+}>()
+</script>
+
+<template>
+  <th v-if="header">{{ header }}</th>
+  <td v-if="td">{{ td }}</td>
+</template>
+
+<style lang="scss">
+th {
+  background-color: black;
+  color: white;
+  text-align: left;
+  text-transform: uppercase;
+
+  &:not(:last-of-type) {
+    border-right: 1px solid;
+  }
+}
+
+th, td {
+  padding: 0.5rem;
+}
+
+tr:nth-child(odd) {
+  border-bottom: 1px solid black;
+  border-left: 1px solid black;
+  border-right: 1px solid black;
+}
+
+td:not(:last-of-type) {
+  border-right: 1px solid;;
+}
+</style>
