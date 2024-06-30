8a992dcff1768b541b0e90f8795c0a918b9368f3
P.E. Butler III <p.e.butler3@gmail.com>
2024-06-29 23:40:11 -0600

    feat: add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import './assets/styles/theme.scss';
 import './style.css'
 import './assets/styles/styles.scss';
+import { inject } from '@vercel/analytics';
 import App from './App.vue'
 
+inject({
+  debug: false,
+});
 createApp(App).mount('#app')
